a6eaa3709b8525d8ad2e474d1501d5e72545d985
Kishor <kishor@seas.upenn.edu>
2020-11-14 17:47:57 -0500

    plot_trajectories_crashing

--- simulator/decomposed_mode_predictor/plot_trajectories.py
+++ simulator/decomposed_mode_predictor/plot_trajectories.py
@@ -1,20 +1,21 @@
+from enum import Enum
+import yaml
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
+import random
+import numpy as np
+from Car import complex_track
+from Car import T_hall_right
+from Car import triangle_hall_equilateral_right
+from Car import triangle_hall_sharp_right
+from Car import trapezoid_hall_sharp_right
+from Car import square_hall_left
+from Car import square_hall_right
+from Car import World
 import os
 import sys
 sys.path.append('..')
-from Car import World
-from Car import square_hall_right
-from Car import square_hall_left
-from Car import trapezoid_hall_sharp_right
-from Car import triangle_hall_sharp_right
-from Car import triangle_hall_equilateral_right
-from Car import T_hall_right
-from Car import complex_track
-import numpy as np
-import random
-from tensorflow.keras import models
-import matplotlib.pyplot as plt
-import yaml
-from enum import Enum
+
 
 class Modes(Enum):
     STRAIGHT = 'STRAIGHT'
@@ -23,12 +24,14 @@ class Modes(Enum):
     SHARP_RIGHT = 'SHARP_RIGHT'
     SHARP_LEFT = 'SHARP_LEFT'
 
+
 def sigmoid(x):
 
     sigm = 1. / (1. + np.exp(-x))
 
     return sigm
-    
+
+
 def int2mode(i):
     if i == 0:
         return Modes.STRAIGHT
@@ -43,6 +46,7 @@ def int2mode(i):
     else:
         raise ValueError
 
+
 class ComposedModePredictor:
     def __init__(self, big_file,
                  straight_file, square_right_file, square_left_file,
@@ -52,37 +56,37 @@ class ComposedModePredictor:
 
         if yml:
             with open(big_file, 'rb') as f:
-                self.big = yaml.load(f, Loader=yaml.CLoader)
+                self.big = yaml.load(f)
 
             self.little = {}
             with open(straight_file, 'rb') as f:
-                self.little[Modes.STRAIGHT] = yaml.load(f, Loader=yaml.CLoader)
+                self.little[Modes.STRAIGHT] = yaml.load(f)
             with open(square_right_file, 'rb') as f:
-                self.little[Modes.SQUARE_RIGHT] = yaml.load(f, Loader=yaml.CLoader)
+                self.little[Modes.SQUARE_RIGHT] = yaml.load(f)
             with open(square_left_file, 'rb') as f:
-                self.little[Modes.SQUARE_LEFT] = yaml.load(f, Loader=yaml.CLoader)
+                self.little[Modes.SQUARE_LEFT] = yaml.load(f)
             with open(sharp_right_file, 'rb') as f:
-                self.little[Modes.SHARP_RIGHT] = yaml.load(f, Loader=yaml.CLoader)
+                self.little[Modes.SHARP_RIGHT] = yaml.load(f)
             with open(sharp_left_file, 'rb') as f:
-                self.little[Modes.SHARP_LEFT] = yaml.load(f, Loader=yaml.CLoader)                
+                self.little[Modes.SHARP_LEFT] = yaml.load(f)
         else:
-        
+
             self.big = models.load_model(big_file)
             self.little = {
-                    Modes.STRAIGHT: models.load_model(straight_file),
-                    Modes.SQUARE_RIGHT: models.load_model(square_right_file),
-                    Modes.SQUARE_LEFT: models.load_model(square_left_file),
-                    Modes.SHARP_RIGHT: models.load_model(sharp_right_file),
-                    Modes.SHARP_LEFT: models.load_model(sharp_left_file)
-                    }
+                Modes.STRAIGHT: models.load_model(straight_file),
+                Modes.SQUARE_RIGHT: models.load_model(square_right_file),
+                Modes.SQUARE_LEFT: models.load_model(square_left_file),
+                Modes.SHARP_RIGHT: models.load_model(sharp_right_file),
+                Modes.SHARP_LEFT: models.load_model(sharp_left_file)
+            }
         self.current_mode = Modes.STRAIGHT
 
     def predict(self, observation):
         obs = observation.reshape(1, -1)
 
         if self.yml:
-            #print(self.current_mode)
-            #print(predict(self.little[self.current_mode], obs))
+            # print(self.current_mode)
+            # print(predict(self.little[self.current_mode], obs))
             if predict(self.little[self.current_mode], obs).round()[0] > 0.5:
                 self.current_mode = int2mode(np.argmax(predict(self.big, obs)))
         else:
@@ -90,12 +94,13 @@ class ComposedModePredictor:
                 self.current_mode = int2mode(np.argmax(self.big.predict(obs)))
         return self.current_mode
 
+
 class ComposedSteeringPredictor:
     def __init__(self, square_file, sharp_file, action_scale):
         with open(square_file, 'rb') as f:
-            self.square_ctrl = yaml.load(f, Loader=yaml.CLoader)
+            self.square_ctrl = yaml.load(f)
         with open(sharp_file, 'rb') as f:
-            self.sharp_ctrl = yaml.load(f, Loader=yaml.CLoader)
+            self.sharp_ctrl = yaml.load(f)
         self.action_scale = action_scale
 
     def predict(self, observation, mode):
@@ -107,6 +112,7 @@ class ComposedSteeringPredictor:
             delta = -delta
         return delta
 
+
 def predict(model, inputs):
     weights = {}
     offsets = {}
@@ -115,7 +121,7 @@ def predict(model, inputs):
     activations = []
 
     for layer in range(1, len(model['weights']) + 1):
-        
+
         weights[layer] = np.array(model['weights'][layer])
         offsets[layer] = np.array(model['offsets'][layer])
 
@@ -133,12 +139,14 @@ def predict(model, inputs):
         elif 'Tanh' in activations[layer]:
             curNeurons = np.tanh(curNeurons)
 
-    return curNeurons    
+    return curNeurons
+
 
 def normalize(s):
     mean = [2.5]
     spread = [5.0]
     return (s - mean) / spread
+
 
 def reverse_lidar(data):
     new_data = np.zeros((data.shape))
@@ -148,9 +156,10 @@ def reverse_lidar(data):
 
     return new_data
 
+
 def main(argv):
-    
-    numTrajectories = 100
+
+    numTrajectories = 1
 
     # mode_predictor = ComposedModePredictor(
     #         'big.h5', 'straight_little.h5',
@@ -159,20 +168,20 @@ def main(argv):
     #         )
 
     mode_predictor = ComposedModePredictor(
-            'big.yml', 'straight_little.yml',
-            'square_right_little.yml', 'square_left_little.yml',
-            'sharp_right_little.yml', 'sharp_left_little.yml', True)    
+        'big.yml', 'straight_little.yml',
+        'square_right_little.yml', 'square_left_little.yml',
+        'sharp_right_little.yml', 'sharp_left_little.yml', True)
 
-    (hallWidths, hallLengths, turns) = T_hall_right(1.5)
+    (hallWidths, hallLengths, turns) = triangle_hall_sharp_right(1.5)
     #(hallWidths, hallLengths, turns) = square_hall_left(1.5)
     #(hallWidths, hallLengths, turns) = triangle_hall_equilateral_right(1.5)
     #(hallWidths, hallLengths, turns) = complex_track(1.5)
-    
-    car_dist_s = hallWidths[0]/2.0
-    car_dist_f = 6.5
-    car_heading = 0.005
+
+    car_dist_s = 0.8231258620067857
+    car_dist_f = 12.072414508350143
+    car_heading = 0.0013037834416212432
     car_V = 2.4
-    episode_length = 400
+    episode_length = 55
     time_step = 0.1
 
     state_feedback = False
@@ -188,20 +197,20 @@ def main(argv):
 
     num_unsafe = 0
 
-    w = World(hallWidths, hallLengths, turns,\
-              car_dist_s, car_dist_f, car_heading, car_V,\
-              episode_length, time_step, lidar_field_of_view,\
+    w = World(hallWidths, hallLengths, turns,
+              car_dist_s, car_dist_f, car_heading, car_V,
+              episode_length, time_step, lidar_field_of_view,
               lidar_num_rays, lidar_noise, missing_lidar_rays, True, state_feedback=state_feedback)
 
     throttle = 16
     action_scale = float(w.action_space.high[0])
 
     steering_ctrl = ComposedSteeringPredictor(
-            os.path.join('..', '..', 'verisig', 'tanh64x64_right_turn_lidar.yml'),
-            os.path.join('..', '..', 'verisig', 'tanh64x64_sharp_turn_lidar.yml'),
-            action_scale
-            )
-    
+        os.path.join('..', '..', 'verisig', 'tanh64x64_right_turn_lidar.yml'),
+        os.path.join('..', '..', 'verisig', 'tanh64x64_sharp_turn_lidar.yml'),
+        action_scale
+    )
+
     allX = []
     allY = []
     allR = []
@@ -218,8 +227,9 @@ def main(argv):
     sharp_left_pred_y = []
 
     # initial uncertainty
-    init_pos_noise = 0.1
-    init_heading_noise = 0.005
+    init_pos_noise = 0
+    init_heading_noise = 0
+    init_y_noise = 0
     it = 0
 
     min_s = 10
@@ -231,14 +241,15 @@ def main(argv):
 
     for step in range(numTrajectories):
 
-        observation = w.reset(pos_noise = init_pos_noise, heading_noise = init_heading_noise)
+        observation = w.reset(pos_noise=init_pos_noise,
+                              heading_noise=init_heading_noise, y_noise=init_y_noise)
 
         init_cond = [w.car_dist_f, w.car_dist_s, w.car_heading, w.car_V]
 
         rew = 0
 
         for e in range(episode_length):
-            
+
             if not state_feedback:
                 observation = normalize(observation)
 
@@ -246,10 +257,10 @@ def main(argv):
 
             if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
                 observation = reverse_lidar(observation)
-            
+
             allX.append(w.car_global_x)
             allY.append(w.car_global_y)
-            
+
             delta = steering_ctrl.predict(observation, mode)
 
             if mode == Modes.STRAIGHT:
@@ -267,19 +278,20 @@ def main(argv):
             elif mode == Modes.SHARP_LEFT:
                 sharp_left_pred_x.append(w.car_global_x)
                 sharp_left_pred_y.append(w.car_global_y)
-            
+
             observation, reward, done, info = w.step(delta, throttle)
 
             if done:
-                
+
                 if e < episode_length - 1:
                     num_unsafe += 1
+                    print('Crash starting at: {}'.format(init_cond))
 
                 if w.car_dist_s > max_s:
                     max_s = w.car_dist_s
                 if w.car_dist_s < min_s:
                     min_s = w.car_dist_s
-                    
+
                 if w.car_dist_f > max_f:
                     max_f = w.car_dist_f
                 if w.car_dist_f < min_f:
@@ -289,7 +301,7 @@ def main(argv):
                     max_h = w.car_heading
                 if w.car_heading < min_h:
                     min_h = w.car_heading
-                                
+
                 break
 
             rew += reward
@@ -299,11 +311,11 @@ def main(argv):
     print('h bounds: [' + str(min_h) + ', ' + str(max_h) + ']')
 
     print('number of crashes: ' + str(num_unsafe))
-    #print(it)
-    fig = plt.figure(figsize=(12,10))
+    # print(it)
+    fig = plt.figure(figsize=(12, 10))
     w.plotHalls()
-    
-    #plt.ylim((-1,11))
+
+    # plt.ylim((-1,11))
     # plt.xlim((-1.75,15.25))
     # plt.tick_params(labelsize=20)
 
@@ -319,9 +331,10 @@ def main(argv):
     # for i in range(numTrajectories):
     #     plt.plot(allX[i], allY[i], 'r-')
 
-    #plt.show()
+    plt.show()
     plt.legend(markerscale=10)
     plt.savefig('trajectories.png')
-    
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
