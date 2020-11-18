2f0a0dd3d11d6726eca820fa632622289ed3c8b3
Kishor <kishor@seas.upenn.edu>
2020-11-17 22:00:03 -0500

    added generation if implication examples

---
+++ simulator/decomposed_mode_predictor/synthesis.py
@@ -0,0 +1,271 @@
+import numpy as np
+import yaml
+import os
+
+from enum import Enum
+from tensorflow.keras import models
+
+from Car import triangle_hall_equilateral_right
+from Car import square_hall_right
+from Car import World
+
+
+class Modes(Enum):
+    STRAIGHT = 'STRAIGHT'
+    SQUARE_RIGHT = 'SQUARE_RIGHT'
+    SQUARE_LEFT = 'SQUARE_LEFT'
+    SHARP_RIGHT = 'SHARP_RIGHT'
+    SHARP_LEFT = 'SHARP_LEFT'
+
+
+SYNTH_MODES = [Modes.SQUARE_RIGHT, Modes.SHARP_RIGHT, Modes.SQUARE_LEFT, Modes.SHARP_LEFT]
+
+
+def sigmoid(x):
+
+    sigm = 1. / (1. + np.exp(-x))
+
+    return sigm
+
+
+def int2mode(i):
+    if i == 0:
+        return Modes.STRAIGHT
+    elif i == 1:
+        return Modes.SQUARE_RIGHT
+    elif i == 2:
+        return Modes.SQUARE_LEFT
+    elif i == 3:
+        return Modes.SHARP_RIGHT
+    elif i == 4:
+        return Modes.SHARP_LEFT
+    else:
+        raise ValueError
+
+
+class ComposedModePredictor:
+    def __init__(self, big_file,
+                 straight_file, square_right_file, square_left_file,
+                 sharp_right_file, sharp_left_file, yml=False):
+
+        self.yml = yml
+
+        if yml:
+            with open(big_file, 'rb') as f:
+                self.big = yaml.load(f)
+
+            self.little = {}
+            with open(straight_file, 'rb') as f:
+                self.little[Modes.STRAIGHT] = yaml.load(f)
+            with open(square_right_file, 'rb') as f:
+                self.little[Modes.SQUARE_RIGHT] = yaml.load(f)
+            with open(square_left_file, 'rb') as f:
+                self.little[Modes.SQUARE_LEFT] = yaml.load(f)
+            with open(sharp_right_file, 'rb') as f:
+                self.little[Modes.SHARP_RIGHT] = yaml.load(f)
+            with open(sharp_left_file, 'rb') as f:
+                self.little[Modes.SHARP_LEFT] = yaml.load(f)
+        else:
+
+            self.big = models.load_model(big_file)
+            self.little = {
+                Modes.STRAIGHT: models.load_model(straight_file),
+                Modes.SQUARE_RIGHT: models.load_model(square_right_file),
+                Modes.SQUARE_LEFT: models.load_model(square_left_file),
+                Modes.SHARP_RIGHT: models.load_model(sharp_right_file),
+                Modes.SHARP_LEFT: models.load_model(sharp_left_file)
+            }
+        self.current_mode = Modes.STRAIGHT
+
+    def predict(self, observation):
+        obs = observation.reshape(1, -1)
+
+        if self.yml:
+            if predict(self.little[self.current_mode], obs).round()[0] > 0.5:
+                self.current_mode = int2mode(np.argmax(predict(self.big, obs)))
+        else:
+            if self.little[self.current_mode].predict(obs).round()[0] > 0.5:
+                self.current_mode = int2mode(np.argmax(self.big.predict(obs)))
+        return self.current_mode
+
+
+class ComposedSteeringPredictor:
+    def __init__(self, square_file, sharp_file, action_scale):
+        with open(square_file, 'rb') as f:
+            self.square_ctrl = yaml.load(f)
+        with open(sharp_file, 'rb') as f:
+            self.sharp_ctrl = yaml.load(f)
+        self.action_scale = action_scale
+
+    def predict(self, observation, mode):
+        if mode == Modes.STRAIGHT or mode == Modes.SQUARE_RIGHT or mode == Modes.SQUARE_LEFT:
+            delta = self.action_scale * predict(self.square_ctrl, observation.reshape(1, -1))
+        else:
+            delta = self.action_scale * predict(self.sharp_ctrl, observation.reshape(1, -1))
+        if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
+            delta = -delta
+        return delta
+
+
+def predict(model, inputs):
+    weights = {}
+    offsets = {}
+
+    layerCount = 0
+    activations = []
+
+    for layer in range(1, len(model['weights']) + 1):
+
+        weights[layer] = np.array(model['weights'][layer])
+        offsets[layer] = np.array(model['offsets'][layer])
+
+        layerCount += 1
+        activations.append(model['activations'][layer])
+
+    curNeurons = inputs
+
+    for layer in range(layerCount):
+
+        curNeurons = curNeurons.dot(weights[layer + 1].T) + offsets[layer + 1]
+
+        if 'Sigmoid' in activations[layer]:
+            curNeurons = sigmoid(curNeurons)
+        elif 'Tanh' in activations[layer]:
+            curNeurons = np.tanh(curNeurons)
+
+    return curNeurons
+
+
+def normalize(s):
+    mean = [2.5]
+    spread = [5.0]
+    return (s - mean) / spread
+
+
+def reverse_lidar(data):
+    new_data = np.zeros((data.shape))
+    for i in range(len(data)):
+        new_data[i] = data[len(data) - i - 1]
+    return new_data
+
+
+def get_dist_f(mode):
+    if mode == Modes.SQUARE_RIGHT or mode == Modes.SQUARE_LEFT:
+        car_dist_f = 6.5
+        car_exit_f = 13.5
+    if mode == Modes.SHARP_RIGHT or mode == Modes.SHARP_LEFT:
+        car_dist_f = 8.0
+        car_exit_f = 12.0
+    return car_dist_f, car_exit_f
+
+
+def get_implications(num_trajectories, mode,
+                     mode_predictor, num_modes=4):
+
+    if mode == Modes.SQUARE_RIGHT or mode == Modes.SQUARE_LEFT:
+        (hallWidths, hallLengths, turns) = square_hall_right(1.5)
+    if mode == Modes.SHARP_RIGHT or mode == Modes.SHARP_LEFT:
+        (hallWidths, hallLengths, turns) = triangle_hall_equilateral_right(1.5)
+
+    car_dist_s = 0.75
+    car_heading = 0
+    car_V = 2.4
+    car_dist_f, car_exit_f = get_dist_f(mode)
+
+    episode_length = 100
+    time_step = 0.1
+
+    state_feedback = False
+    lidar_field_of_view = 115
+    lidar_num_rays = 21
+
+    # Change this to 0.1 or 0.2 to generate Figure 3 or 5 in the paper, respectively
+    lidar_noise = 0
+
+    # Change this to 0 or 5 to generate Figure 3 or 5 in the paper, respectively
+    missing_lidar_rays = 0
+
+    w = World(hallWidths, hallLengths, turns,
+              car_dist_s, car_dist_f, car_heading, car_V,
+              episode_length, time_step, lidar_field_of_view,
+              lidar_num_rays, lidar_noise, missing_lidar_rays, True, state_feedback=state_feedback)
+
+    action_scale = float(w.action_space.high[0])
+    steering_ctrl = ComposedSteeringPredictor(
+        os.path.join('..', '..', 'verisig', 'tanh64x64_right_turn_lidar.yml'),
+        os.path.join('..', '..', 'verisig', 'tanh64x64_sharp_turn_lidar.yml'),
+        action_scale
+    )
+    throttle = 16
+
+    entry_set = []
+    exit_set = []
+
+    # initial uncertainty
+    init_pos_noise = 0.1
+    init_heading_noise = 0.006
+    init_y_noise = 0.25
+
+    for step in range(num_trajectories):
+
+        if step == 0:
+            observation = w.reset(pos_noise=0, heading_noise=0, y_noise=0)
+        else:
+            observation = w.reset(pos_noise=init_pos_noise,
+                                  heading_noise=init_heading_noise, y_noise=init_y_noise)
+
+        init_cond = np.array([w.car_dist_s, car_dist_f - w.car_dist_f, w.car_heading])
+        if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
+            init_cond[0] = 1.5 - init_cond[0]
+            init_cond[2] = -init_cond[2]
+
+        mode_predictor.current_mode = Modes.STRAIGHT
+
+        for e in range(episode_length):
+
+            if not state_feedback:
+                observation = normalize(observation)
+            if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
+                observation = reverse_lidar(observation)
+
+            predicted_mode = mode_predictor.predict(observation)
+
+            if predicted_mode == Modes.SQUARE_LEFT or predicted_mode == Modes.SHARP_LEFT:
+                observation = reverse_lidar(observation)
+
+            delta = steering_ctrl.predict(observation, predicted_mode)
+            if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
+                delta = -delta
+
+            observation, _, done, _ = w.step(delta, throttle)
+
+            if done:
+                if e < episode_length - 1:
+                    print('Crash starting at: {}'.format(init_cond.tolist()))
+                break
+
+            elif w.ax_changed and w.car_dist_f <= car_exit_f:
+                entry_set.append(init_cond)
+                exit_cond = np.array([w.car_dist_s, car_exit_f - w.car_dist_f, w.car_heading])
+                if mode == Modes.SQUARE_LEFT or mode == Modes.SHARP_LEFT:
+                    exit_cond[0] = 1.5 - exit_cond[0]
+                    exit_cond[2] = -exit_cond[2]
+                exit_set.append(exit_cond)
+                break
+
+    implications = []
+    for i in range(len(entry_set)):
+        implications.append(((mode, entry_set[i]), (mode, exit_set[i]), 'dynamics'))
+        for m in SYNTH_MODES:
+            implications.append(((mode, exit_set[i]), (m, exit_set[i]), 'transition'))
+
+    return implications
+
+
+if __name__ == '__main__':
+    mode_predictor = ComposedModePredictor(
+        'big.yml', 'straight_little.yml',
+        'square_right_little.yml', 'square_left_little.yml',
+        'sharp_right_little.yml', 'sharp_left_little.yml', True)
+    for imp in get_implications(5, Modes.SQUARE_LEFT, mode_predictor):
+        print((imp[0][1].tolist(), imp[1][1].tolist(), imp[2]))
